79b23e92892478c2fe2a989a8f6fc39710740097
Erica Fagioli <efagioli01@gmail.com>
2021-05-22 09:42:55 -0400

    removed comments

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); //allows us to pass data to and fro routes in json format
-app.use(express.urlencoded({ extended: true })); //allows it to func in browser
-app.use(express.static("public"));  //allows us to access what is in public foldder, html, css, all statoc files
-app.use("/api", apiRoutes) //setting up express to use api routes 
-app.use("/", htmlRoutes) //lets express know whatever port running to display html
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static("public"));  
+app.use("/api", apiRoutes) 
+app.use("/", htmlRoutes) 
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
